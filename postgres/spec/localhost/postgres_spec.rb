0bd2b1e7b5aa0ca8a845953a21266bcc493b4c74
Archie Brentano <isntall.us@gmail.us>
2015-02-15 09:49:49 -0800

    updated to use ubuntu 14.04

--- postgres/spec/localhost/postgres_spec.rb
+++ postgres/spec/localhost/postgres_spec.rb
@@ -12,7 +12,7 @@ describe package('postgresql-contrib') do
   it { should be_installed }
 end
 
-describe file('/etc/postgresql/9.1/main/postgresql.conf') do
+describe file('/etc/postgresql/9.3/main/postgresql.conf') do
   it { should be_file }
   it { should contain "listen_addresses='*'" }
 end
